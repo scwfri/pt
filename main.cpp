f09a331f7d11068e413da9970da72b5bb490fd9e
scwfri <scwfri@gmail.com>
2022-01-06 16:26:22 -0600

    Main: update main to incorporate Color class

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 
-int main() {
-  int const image_width = 256;
-  int const image_height = 256;
+#include "maths/Color.h"
+#include "maths/Vec3.h"
 
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+int main()
+{
+    int const image_width = 256;
+    int const image_height = 256;
 
-  for (int j = image_height - 1; j >= 0; --j) {
-    for (int i = 0; i < image_width; ++i) {
-      auto r = double(i) / (image_width - 1);
-      auto g = double(j) / (image_height - 1);
-      auto b = 0.25;
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
 
-      auto ir = static_cast<int>(255.999 * r);
-      auto ig = static_cast<int>(255.999 * g);
-      auto ib = static_cast<int>(255.999 * b);
+    for (int j = image_height - 1; j >= 0; --j) {
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.25;
 
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            Color pixel_color = { r, g, b };
+            write_color(std::cout, pixel_color);
+        }
     }
-  }
-
 }
