bd039ae9eb132fa9a20cbd14ad650f979d74aa50
scwfri <scwfri@gmail.com>
2022-01-20 16:56:51 -0600

    Color: Add gammac correction

--- Maths/Color.cpp
+++ Maths/Color.cpp
@@ -1,18 +1,18 @@
 #include "Color.h"
 
-void write_color(std::ostream& out, Color const& pixel_color, int const samples_per_pixel)
+void write_color(std::ostream& out, Color const& pixel_color, int samples_per_pixel)
 {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r *= sqrt(scale * r);
+    g *= sqrt(scale * g);
+    b *= sqrt(scale * b);
 
     // write (0, 255) value of each pixel_color component
-    out << static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << ' '
-        << static_cast<int>(255.999 * clamp(g, 0.0, 0.999)) << ' '
-        << static_cast<int>(255.999 * clamp(b, 0.0, 0.999)) << '\n';
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
