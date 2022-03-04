01e0231b4ea34df15f094adf75de93d67bd09a5e
scwfri <scwfri@gmail.com>
2022-03-03 20:26:10 -0600

    Meta: Update run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/bash
 set -e
 
+setup() {
+    if [ ! -d "./build" ]; then
+        mkdir ./build
+        cd build && cmake .. && cd -
+    fi
+    if [ ! -d "./out" ]; then
+        mkdir ./out
+    fi
+}
+
 make_exec() {
     if command -v compiledb &> /dev/null
     then
-        compiledb make -j5
+        cd ./build && compiledb make -j5 && cd -
     else
-        make -j5
+        cd ./build && make -j5 && cd -
     fi
 }
 
 gen_image() {
-    ./ray-tracer-5k > ./out/out.ppm
+    ./build/ray-tracer-5k > ./out/out.ppm
 }
 
 view_image() {
     gimp ./out/out.ppm
 }
 
+setup
 make_exec
 gen_image
 view_image
